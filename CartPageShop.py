2f612e6420a770a4991e99d7590f1c1d2eaa370d
vlasovserjj <vlasov.serjj.1360925@gmail.com>
2025-10-06 00:14:33 +0400

    Delete CartPageShop.py

--- CartPageShop.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class Cart:
-    def __init__(self, driver):
-        self._driver = driver
-        self._driver.implicitly_wait(4)
-
-    def click_chek(self):
-        self._driver.find_element(By.CSS_SELECTOR, "#checkout").click()
